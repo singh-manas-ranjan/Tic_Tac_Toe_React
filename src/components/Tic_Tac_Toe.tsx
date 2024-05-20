1c2ead7541969e39238444fe4cf00890ddaa091b
Manas Ranjan Singh <manassingh900@gmail.com>
2024-05-20 20:34:49 +0530

    update_winning_logic_using_useEffect

--- src/components/Tic_Tac_Toe.tsx
+++ src/components/Tic_Tac_Toe.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Tic_Tac_Toe.module.css";
 const Tic_Tac_Toe = () => {
   const [options, setOptions] = useState<string[]>(Array(9).fill(""));
   const [currentPlayer, setCurrentPlayer] = useState<string>("X");
   const [running, setRunning] = useState<boolean>(false);
+  const [winner, setWinner] = useState<string>("");
+  const [draw, setDraw] = useState<boolean>(false);
+
+  useEffect(() => {
+    checkWinner();
+  });
 
   const winConditions: number[][] = [
     [0, 1, 2],
@@ -16,27 +22,37 @@ const Tic_Tac_Toe = () => {
     [2, 4, 6],
   ];
 
-  const startGame = () => {
-    setRunning(true);
-  };
+  const checkWinner = () => {
+    for (const combination of winConditions) {
+      const [cellA, cellB, cellC] = combination;
 
-  const checkWinner = (options: string[]) => {
-    for (let i = 0; i < winConditions.length; i++) {
-      const combination = winConditions[i];
-      const [a, b, c] = combination;
-
-      if (options[a] === "" || options[b] === "" || options[c] === "") continue;
-      if (options[a] === options[b] && options[b] === options[c]) {
+      if (
+        options[cellA] === "" ||
+        options[cellB] === "" ||
+        options[cellC] === ""
+      ) {
+        continue;
+      }
+      if (
+        options[cellA] === options[cellB] &&
+        options[cellB] === options[cellC]
+      ) {
         setRunning(false);
-        return true;
+        setWinner(currentPlayer === "X" ? "O" : "X");
+        break;
+      } else if (!options.includes("")) {
+        setRunning(false);
+        setDraw(true);
       }
     }
   };
 
+  const startGame = () => {
+    setRunning(true);
+  };
+
   const cellClicked = (cellIdx: number) => {
     if (options[cellIdx] !== "" || !running) return;
-    const win = checkWinner(options);
-    if (win) alert(`${currentPlayer} Wins!`);
     updateCell(cellIdx);
   };
 
@@ -44,6 +60,10 @@ const Tic_Tac_Toe = () => {
     const newOptions = [...options];
     newOptions[cellIdx] = currentPlayer;
     setOptions(newOptions);
+    changePlayer();
+  };
+
+  const changePlayer = () => {
     setCurrentPlayer((player) => (player === "X" ? "O" : "X"));
   };
 
@@ -51,6 +71,8 @@ const Tic_Tac_Toe = () => {
     setOptions(Array(9).fill(""));
     setCurrentPlayer("X");
     setRunning(false);
+    setWinner("");
+    setDraw(false);
   };
 
   return (
@@ -69,9 +91,29 @@ const Tic_Tac_Toe = () => {
       </div>
       <h2>{running ? `${currentPlayer}'s turn` : ""}</h2>
       <div className={running ? styles.hidden : styles.curtain}>
-        <h1>Tic-Tac-Toe</h1>
-        <button onClick={startGame}>Start Game</button>
-        <button onClick={resetGame} className={running ? "" : styles.hidden}>
+        <h1>
+          {winner !== ""
+            ? `${winner} Wins!`
+            : draw
+            ? "It's a draw"
+            : "Tic-Tac-Toe"}
+        </h1>
+        <button
+          onClick={startGame}
+          className={
+            options.includes("X") || options.includes("O") ? styles.hidden : ""
+          }
+        >
+          Start Game
+        </button>
+        <button
+          onClick={resetGame}
+          className={
+            running || !options.includes("X") || !options.includes("O")
+              ? styles.hidden
+              : ""
+          }
+        >
           Reset Board
         </button>
       </div>
